8f8c36bfdead04d464d9c44966b229510fd473b0
bankkeez <sethitikul@gmail.com>
2018-04-23 15:31:19 +0700

    TODO switch scene

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,27 +8,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 public class Main extends Application {
+	Scene scene;
+
 	@Override
 	public void start(Stage stage) {
 		try {
-			/*
-			 * Parent object for all nodes that have children in the scene graph
-			 * (Anonymous Inner Type)
-			 */
-			Parent root = new Parent() {
-			};
-			Scene scene = new Scene(root);
-			// ScreenController object for switching scenes
-			ScreenController screenController = new ScreenController(scene);
-			screenController.addScene("customer-table", FXMLLoader.load(getClass().getResource("customer-table.fxml")));
-			screenController.addScene("customer-menu", FXMLLoader.load(getClass().getResource("customer-menu.fxml")));
-			// Set the scene
-			screenController.activate("customer-table");
-			// Show the scene
-			stage.setScene(scene);
-			stage.sizeToScene();
-			stage.setResizable(false);
-			stage.show();
+
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/controller/ScreenController.java
+++ src/controller/ScreenController.java
@@ -14,41 +14,5 @@ import javafx.stage.Stage;
  *
  */
 public class ScreenController {
-	private Stage stage;
-	private Map<String, Pane> sceneMap = new HashMap<>();
-	private Scene scene;
-
-	public ScreenController(Scene scene) {
-		this.scene = scene;
-	}
-
-	/**
-	 * Adds the scene into the Map<K,V> for switching.
-	 * 
-	 * @param fxmlname
-	 * @param pane
-	 */
-	public void addScene(String fxmlname, Pane pane) {
-		sceneMap.put(fxmlname, pane);
-	}
-
-	/**
-	 * Removes the scene from the Map<K,V>.
-	 * 
-	 * @param fxmlname
-	 */
-	public void removeScreen(String fxmlname) {
-		sceneMap.remove(fxmlname);
-	}
-
-	/**
-	 * Set the scene from fxmlname.
-	 * 
-	 * @param fxmlname
-	 */
-	public void activate(String fxmlname) {
-		//setRoot for defining root node of scene graph
-		scene.setRoot(sceneMap.get(fxmlname));
-	}
 
 }
