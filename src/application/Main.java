76612e3cca20b93b3626212c34e9ca2a3515fa85
bankkeez <sethitikul@gmail.com>
2018-04-24 19:16:41 +0700

    switch scene successful

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 
 import javafx.application.Application;
@@ -12,7 +13,21 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		
+		try {
+			String fxmlfile = "login.fxml";
+			URL url = getClass().getResource(fxmlfile);
+			FXMLLoader loader = new FXMLLoader(url);
+			Parent root;
+			root = loader.load();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.sizeToScene();
+			stage.setTitle("Login");
+			stage.setResizable(false);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,5 +1,16 @@
 package controller;
 
-public class LoginController {
+import application.CSTable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+public class LoginController {
+	@FXML
+	private Button login;
+
+	public void loginButtonHandler(ActionEvent event) {
+		ScreenController.switchWindow((Stage) login.getScene().getWindow(), new CSTable());
+	}
 }
