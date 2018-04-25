cd736fffb17a80f6d8a0c1b1d76701cef05c3f1d
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2018-04-25 21:42:01 +0700

    add function

--- src/controller/CSMenuController.java
+++ src/controller/CSMenuController.java
@@ -1,5 +1,22 @@
 package controller;
 
-public class CSMenuController {
+import application.CSTable;
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+public class CSMenuController {
+	@FXML
+	private Button back;
+
+	/**
+	 * Handler for back button. When event receive the CS table scene is shown.
+	 * 
+	 * @param event
+	 */
+	public void backButtonHandler(ActionEvent event) {
+		ScreenController.switchWindow((Stage) back.getScene().getWindow(), new CSTable());
+	}
 }
