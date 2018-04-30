f69d5a3ffb08367cf62f383856baebd16fdd4cb7
kimvcp <kim-ft@hotmail.com>
2018-05-01 00:36:54 +0700

    fix changes

--- src/controller/CSMenuController.java
+++ src/controller/CSMenuController.java
@@ -1,10 +1,19 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.TabableView;
+
 import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 /**
@@ -16,17 +25,27 @@ import javafx.scene.control.TableView;
  */
 public class CSMenuController {
 	@FXML
-	private Button send;
+	private Button order;
 	@FXML
 	private Button pay;
 	@FXML
 	private Label totalPrice;
 	@FXML
-	private TableView<String> table;
-	@FXML
+	private TableView table;
+
+	private SimpleStringProperty id = new SimpleStringProperty("");
+	private SimpleStringProperty name = new SimpleStringProperty("");
+	private SimpleStringProperty quantity = new SimpleStringProperty("");
+	private SimpleStringProperty price = new SimpleStringProperty("");
 	private ListView<Label> foodList;
-	@FXML
 	private ListProperty<Label> listProperty;
+	List<String> nameCollector = new ArrayList<>();
+
+	@FXML
+	public void initialize() {
+//		id.set(FXCollections.observableList(nameCollector);
+//		listItems.itemsProperty().bind(listProperty);
+	}
 
 	public void sendButtonHandler() {
 		int total = Integer.parseInt(totalPrice.getText());
