0738d07a8f664a2ce8e4f230cb57e8b3684d3db6
kimvcp <kim-ft@hotmail.com>
2018-05-01 21:53:31 +0700

    add food to the model

---
+++ src/model/Food.java
@@ -0,0 +1,41 @@
+package model;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Food {
+	private final SimpleStringProperty name;
+	private final SimpleIntegerProperty quantity;
+	private final SimpleIntegerProperty price;
+
+	public Food(String name, int quantity, int price) {
+		this.name = new SimpleStringProperty(name);
+		this.quantity = new SimpleIntegerProperty(quantity);
+		this.price = new SimpleIntegerProperty(price);
+	}
+
+	public String getName() {
+		return name.get();
+	}
+
+	public void setFirstName(String Name) {
+		name.set(Name);
+	}
+
+	public int getQuantity() {
+		return quantity.get();
+	}
+
+	public void setQuantity(int number) {
+		quantity.set(number);
+	}
+
+	public int getPrice() {
+		return price.get();
+	}
+
+	public void setPrice(int number) {
+		price.set(number);
+	}
+
+}
