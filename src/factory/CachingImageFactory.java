76d65019f28dec5fcf706dc25435a635c0f1ab43
kimvcp <kim-ft@hotmail.com>
2018-05-04 00:06:55 +0700

    add imageFactory

---
+++ src/factory/CachingImageFactory.java
@@ -0,0 +1,17 @@
+package factory;
+
+import java.net.URL;
+
+import com.sun.javafx.collections.MappingChange.Map;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class CachingImageFactory extends ImageFactory {
+	@Override
+	public ImageView getImage(String filename) {
+		
+		return ;
+		
+	}
+}
--- src/factory/ImageFactory.java
+++ src/factory/ImageFactory.java
@@ -1,4 +1,32 @@
 package factory;
+
+import java.net.URL;
+
+import com.sun.org.apache.bcel.internal.util.ClassLoader;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import sun.security.jca.GetInstance;
+
 public class ImageFactory {
+	/** singleton instance of ImageFactory. */
+	protected static ImageFactory factory;
+
+	/**
+	 * Get an instance of ImageFactory.
+	 * 
+	 * @return object of a subclass
+	 */
+	ImageFactory getInstance() {
+		if (factory == null)
+			factory = new ImageFactory();
+		return factory;
+	}
+
+	public ImageView getImage(String filename) {
+		URL url = new ClassLoader().getClass().getResource(filename);
+		Image image = new Image(url.getPath());
+		return new ImageView(image);
+	}
 
 }
