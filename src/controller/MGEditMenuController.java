eba2f942303c0080475b6e699250aadcf65dd4a6
kimvcp <kim-ft@hotmail.com>
2018-05-05 01:05:05 +0700

    test color ui

--- src/controller/MGEditMenuController.java
+++ src/controller/MGEditMenuController.java
@@ -15,7 +15,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextInputDialog;
