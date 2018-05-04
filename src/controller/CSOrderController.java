419509710d4341e35c68a4f098edcd670a6fc7e2
kimvcp <kim-ft@hotmail.com>
2018-05-04 19:25:50 +0700

    update csorder

--- src/controller/CSOrderController.java
+++ src/controller/CSOrderController.java
@@ -1,11 +1,18 @@
 package controller;
 
+import java.util.List;
+
 import application.CSTable;
 import application.Main;
+import database.DBManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 //import model.Food;
 import util.ScreenController;
@@ -27,12 +34,38 @@ public class CSOrderController {
 	private Button exit;
 	@FXML
 	private TextField totalPrice;
+	@FXML
+	private FlowPane foodpane;
+	@FXML
+	private FlowPane drinkpane;
 
 	private static String tablenumber;
+	private static List<String> foodname = DBManager.getFoodname("Foods");
+	private static List<String> drinkname = DBManager.getFoodname("Drinks");
+	private static List<String> urlImage = DBManager.getImageURL("Images");
 
 	@FXML
 	public void initialize() {
-		
+		// adding buttons to foodpane
+		System.out.println(tablenumber);
+		setImage(foodname);
+		setImage(drinkname);
+	}
+
+	public void setImage(List<String> name) {
+		int i = 0;
+		for (String text : foodname) {
+			Button button = new Button(text);
+			Image image = new Image(urlImage.get(i));
+			i++;
+			ImageView view = new ImageView(image);
+			view.setFitHeight(100);
+			view.setFitWidth(100);
+			button.setWrapText(true);
+			button.setTextAlignment(TextAlignment.CENTER);
+			button.setGraphic(view);
+			foodpane.getChildren().add(button);
+		}
 	}
 
 	// during in test
@@ -49,8 +82,7 @@ public class CSOrderController {
 	}
 
 	/**
-	 * Handler for logout button. When event receive the Start up scene is
-	 * shown.
+	 * Handler for logout button. When event receive the Start up scene is shown.
 	 * 
 	 */
 	public void exitButtonHandler(ActionEvent event) {
