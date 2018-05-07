98f35b5c0a4dcb60aa86da735e3c09bd2aefd89c
bankkeez <sethitikul@gmail.com>
2018-05-07 15:56:57 +0700

    rename Class to prevent same java class

--- src/application/Tableview.java
+++ src/application/Tableview.java
@@ -11,7 +11,7 @@ import util.AbstractWindow;
  * @author Piyawat & Vichaphol
  *
  */
-public class TableView extends AbstractWindow {
+public class Tableview extends AbstractWindow {
 	@Override
 	public void start(Stage stage) {
 		try {
